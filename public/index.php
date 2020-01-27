8b6d2a858bfc81977fe633843cc94dfc83c46f2e
Pierre Emmanuel Tanguy <pierre.emmanuel.tanguy@gmail.com>
2020-01-27 02:05:41 +0000

    forbidden access: bugfix attempt 1

--- public/index.php
+++ public/index.php
@@ -21,7 +21,24 @@ if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? $_ENV['TRUSTED_HOSTS'] ?? false
 }
 
 $kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
-$request = Request::createFromGlobals();
+// $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
 $kernel->terminate($request, $response);
+
+$request = Request::createFromGlobals();
+
+// tell Symfony about your reverse proxy
+Request::setTrustedProxies(
+    // the IP address (or range) of your proxy
+    ['192.0.0.1', '10.0.0.0/8'],
+
+    // trust *all* "X-Forwarded-*" headers
+    Request::HEADER_X_FORWARDED_ALL
+
+    // or, if your proxy instead uses the "Forwarded" header
+    // Request::HEADER_FORWARDED
+
+    // or, if you're using AWS ELB
+    // Request::HEADER_X_FORWARDED_AWS_ELB
+);
